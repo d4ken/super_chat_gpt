b9a5f91c2894ddd4857481f430206a3bb352351d
Daiki Shikencho <112839844+d4ken@users.noreply.github.com>
2023-10-03 20:15:30 +0900

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:super_chat_gpt/main.dart';
+import 'package:gpt_sdk_chatapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
